37b9171c4c12a9a99d9d369f1005288220f90ab5
Adrian Xavier <156139748+ignitecorporation@users.noreply.github.com>
2024-08-06 19:31:18 -0300

    modified:   Estrutura_de_dados_2/aula_01.pas

---
+++ Estrutura_de_dados_2/aula_01.pas
@@ -0,0 +1,13 @@
+{ 
+
+   Matriz de adjacencia
+
+        É uma estrutura de dados do tipo matriz de ordem N aonde as arestas
+        adjacentes aos vértices “colunas e linhas” são representadas pelo
+        número UM, caso contrário os zeros. Havendo laços a representação
+        ocorre na diagonal principal, havendo arestas paralelas a representação
+        ocorre indicando o numero de arestas nos vértices adjacentes.
+
+        Caso o grafo seja dirigido, a representação ocorre do vértice sainte
+         para o vértice entrante
+ }
